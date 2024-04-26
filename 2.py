3f5d9beb64f1341589bfe331cb19400f176c2206
sonic-z <sonic1988@list.ru>
2024-04-26 18:35:58 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,25 +9,25 @@ def flat_generator(list_of_lists):
         else:
             yield item
 
-def test_4():
+def test_2():
 
-    list_of_lists_2 = [
-        [['a'], ['b', 'c']],
-        ['d', 'e', [['f'], 'h'], False],
-        [1, 2, None, [[[[['!']]]]], []]
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
     ]
 
     for flat_iterator_item, check_item in zip(
-            flat_generator(list_of_lists_2),
-            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     ):
 
         assert flat_iterator_item == check_item
 
-    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
-    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
 
 if __name__ == '__main__':
-    test_4()
+    test_2()
