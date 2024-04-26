a7f850df6320a7ded34f9f78936a79c2e20c11f8
sonic-z <sonic1988@list.ru>
2024-04-26 18:36:14 +0700

    Update 3star.py

--- 3star.py
+++ 3star.py
@@ -28,24 +28,23 @@ class FlatIterator:
         else:
             return item
 
+def test_3():
 
-def test_1():
-
-    list_of_lists_1 = [
-        ['a', 'b', 'c'],
-        ['d', 'e', 'f', 'h', False],
-        [1, 2, None]
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
     ]
 
     for flat_iterator_item, check_item in zip(
-            FlatIterator(list_of_lists_1),
-            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
     ):
 
         assert flat_iterator_item == check_item
 
-    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
 
 
 if __name__ == '__main__':
-    test_1()
+    test_3()
